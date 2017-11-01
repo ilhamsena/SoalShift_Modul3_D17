852a33db1a5849dd5ca325a5dda45689e6d9c3d2
ilhamsena <32926512+ilhamsena@users.noreply.github.com>
2017-11-01 22:20:38 +0700

    Create revisi4.c

---
+++ revisi4.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+pthread_t tid[20];
+void *counting(void *faktorial)
+{
+    int i,j,k=1;
+    int a=(int)faktorial;
+    if (a<0)
+    {
+        printf ("Error\n");
+    }
+    else
+    {
+
+        for (i=a;i>=1;i--)
+        {
+            k = k*i;
+        }
+        printf ("hasil %d!= %d",a,k);
+    }
+}
+
+int main()
+{
+    int angka[20],err,faktorial=0,i;
+    char space;
+    while(1)
+    {
+
+        scanf ("%d%c",&angka[faktorial],&space);
+        faktorial++;
+        if (space == ' ')
+        {
+            break;
+        }
+
+    }
+    for (i=0;i<faktorial;i++)
+    {
+        err=pthread_create(&(tid[i]),NULL,&counting,(void*) angka[i]));//membuat thread
+        if(err!=0)//cek error
+        {
+            printf("\n can't create thread : [%d]",err);
+            exit(EXIT_FAILURE);
+        }
+        
+    }
+    for (i=0;i<faktorial;i++)
+    {
+           pthread_join(tid[i],NULL);
+    }
+    exit(EXIT_SUCCESS);
+}
