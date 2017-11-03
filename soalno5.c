214f60d1c7082eecdae91ca02b203d107f8621a8
maidina98 <33053581+maidina98@users.noreply.github.com>
2017-11-03 14:55:35 +0000

    revisi

---
+++ soalno5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+void* findstring(void *arg){
+	int count=0;
+	char *string=(char*)arg;
+	FILE *fp=fopen("Novel.txt","r");
+	char len[256];
+	if(fp==NULL){
+	printf("File not found!\n");
+	} else {
+	while(fscanf(fp,"%s",len)!=EOF)
+	if(!strncmp(len,string,strlen(string)))count++;
+	printf("%s : %d \n", arg, count);
+	}
+}
+
+int main(int argc, char *argv[]){
+
+	pthread_t tid[5];
+
+	int i=0;
+
+	if(argc<2){
+		printf("Masukan minimal 2 argument\n");
+	}
+
+	for(i=1;i<argc;i++)pthread_create(&(tid[i]),NULL,&findstring,(void*) argv[i]);
+	for(i=1;i<argc;i++)pthread_join(tid[i],NULL);
+}
